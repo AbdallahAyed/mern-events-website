0c65e2ffe0661c180095aadf42e4d968360eb90c
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2024-03-23 14:49:34 -0700

    Add header styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "Evently",
   description: "Evently is a platform for event management.",
   icons: {
-    icon: "/assets/logo.svg",
+    icon: "/images/logo.svg",
   },
 };
 
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export const Header = () => {
-  return <header className="w-full border-b"><div className=""></div></header>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently Logo"
+          />
+        </Link>
+      </div>
+    </header>
+  );
 };
