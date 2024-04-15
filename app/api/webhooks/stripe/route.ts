fc0cfe2ea40c399827e855898bcefb1450d88bc5
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2024-04-15 14:51:29 +0200

    feat: Add stripe webhook api route

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import stripe from "stripe";
+
+export async function POST(request: Request) {
+  const body = await request.text();
+
+  const sig = request.headers.get("stripe-signature") as string;
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
+    return NextResponse.json({ message: "Webhook error", error: err });
+  }
+
+  // Get the ID and type
+  const eventType = event.type;
+
+  //TODO: CREATE
+
+  return new Response("", { status: 200 });
+}
